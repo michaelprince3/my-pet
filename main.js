8c114dfb990248d1484f2f401df27897cee8c573
Mike Prince <michaelprince3@gmail.com>
2020-01-20 15:47:41 -0600

    pet sounds

--- main.js
+++ main.js
@@ -3,4 +3,14 @@ const myDog = {
     type: "lab",
     nicknames: ["bubba", "crazy", "lazy"],
     age: 10,
+    bark: function() {
+        console.log("WOOF!")
+    },
+    whine: function() {
+        console.log("whine")
+    },
+    growl: function() {
+        console.log("Growl!")
+    },
+
 }
